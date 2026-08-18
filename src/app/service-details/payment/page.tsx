dd9f1bbba697e023d6a94757ef53a5bf4d26c915
artchapong hassametto <artchapong.hassa@gmail.com>
2026-08-18 16:19:33 +0700

    feat: wrap HeroSectionThree with PaymentProvider in service payment page

--- src/app/service-details/payment/page.tsx
+++ src/app/service-details/payment/page.tsx
@@ -1,10 +1,13 @@
 import NavbarResponsive from "@/components/service-details/navbar-responsive";
 import HeroSectionThree from "@/components/service-details/hero-section-3";
+import { PaymentProvider } from "@/app/service-details/layout";
 
 export default function servicePaymentPage() {
   return (
     <>
-      <HeroSectionThree />
+      <PaymentProvider>
+        <HeroSectionThree />
+      </PaymentProvider>
     </>
   );
 }
