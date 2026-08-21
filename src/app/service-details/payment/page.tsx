adcf690321a3835e1834c41e43c7602c8828e097
artchapong hassametto <artchapong.hassa@gmail.com>
2026-08-21 11:06:02 +0700

    fix: bring back some lost code

--- src/app/service-details/payment/page.tsx
+++ src/app/service-details/payment/page.tsx
@@ -1,9 +1,15 @@
 import HeroSectionThree from "@/components/service-details/hero-section-3";
+import { PaymentProvider } from "@/app/service-details/layout";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 export default function ServicePaymentPage() {
   return (
     <>
-      <HeroSectionThree />
+    <AuthProvider>
+      <PaymentProvider>
+        <HeroSectionThree />
+      </PaymentProvider>
+     </AuthProvider>
     </>
   );
 }
